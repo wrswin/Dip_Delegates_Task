2ac46e6cdb597be1ea42faf1ddd43b4550d607b2
Wayde Reitsma <pootisspencerherenao@gmail.com>
2020-05-03 21:17:13 +1000

    Change DataParser methods to modify in-place

--- FileParser/DataParser.cs
+++ FileParser/DataParser.cs
@@ -10,19 +10,13 @@ namespace FileParser {
         /// <param name="data"></param>
         /// <returns></returns>
         public List<List<string>> StripWhiteSpace(List<List<string>> data) {
-            var result = new List<List<string>>();
-
             foreach(var line in data) {
-                var resultColumns = new List<string>();
-
-                foreach(var column in line) {
-                    resultColumns.Add(column.Trim());
+                for(var i = 0; i < line.Count; i += 1) {
+                    line[i] = line[i].Trim();
                 }
-
-                result.Add(resultColumns);
             }
 
-            return result;
+            return data;
         }
 
         /// <summary>
@@ -31,19 +25,13 @@ namespace FileParser {
         /// <param name="data"></param>
         /// <returns></returns>
         public List<List<string>> StripQuotes(List<List<string>> data) {
-            var result = new List<List<string>>();
-
             foreach(var line in data) {
-                var resultColumns = new List<string>();
-
-                foreach(var column in line) {
-                    resultColumns.Add(column.Trim('"'));
+                for(var i = 0; i < line.Count; i += 1) {
+                    line[i] = line[i].Trim('"');
                 }
-
-                result.Add(resultColumns);
             }
 
-            return result;
+            return data;
         }
 
     }
