eceb4f263b10d1cde4b7e0bf1d96cd08a7cfdd34
Wayde Reitsma <pootisspencerherenao@gmail.com>
2020-05-03 21:12:55 +1000

    Implement CsvHandler class

--- FileParser/CsvHandler.cs
+++ FileParser/CsvHandler.cs
@@ -15,7 +15,13 @@ namespace Delegate_Exercise {
         /// <param name="writeFile"></param>
         /// <param name="dataHandler"></param>
         public void ProcessCsv(string readFile, string writeFile, Func<List<List<string>>, List<List<string>>> dataHandler) {
+            var fileHandler = new FileHandler();
 
+            var inputLines = fileHandler.ParseCsv(fileHandler.ReadFile(readFile));
+
+            var outputLines = dataHandler(inputLines);
+
+            fileHandler.WriteFile(writeFile, ',', outputLines);
         }
         
     }
