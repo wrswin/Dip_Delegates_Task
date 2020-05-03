d711025713ab018ebc563cf7b980d1110a4750b5
Wayde Reitsma <pootisspencerherenao@gmail.com>
2020-05-03 21:39:24 +1000

    Implement Delegate_Exercise project

--- Delegate_Exercise/Program.cs
+++ Delegate_Exercise/Program.cs
@@ -10,7 +10,15 @@ namespace Delegate_Exercise
     {
         static void Main(string[] args)
         {
+            var fileHandler = new FileHandler();
+            var dataParser = new DataParser();
+            var csvHandler = new CsvHandler();
 
+            var dataHandler = new Func<List<List<string>>, List<List<string>>>(dataParser.StripQuotes);
+            dataHandler += dataParser.StripWhiteSpace;
+            dataHandler += RemoveHashes;
+
+            csvHandler.ProcessCsv("TempFiles/data.csv", "TempFiles/processed_data.csv", dataHandler);
         }
 
         public static List<List<string>> RemoveHashes(List<List<string>> data) {
@@ -23,6 +31,19 @@ namespace Delegate_Exercise
             }
             return data;
             
+        }
+
+        public static List<List<string>> RemoveHashesAndCapitalise(List<List<string>> data) {
+            foreach(var row in data) {
+                for (var index = 0; index < row.Count; index++) {
+                    if(row[index][0] == '#') {
+                        row[index] = row[index].Remove(0,1);
+                    }
+
+                    row[index] = row[index][0].ToString().ToUpper() + row[index].Remove(0,1);
+                }
+            }
+            return data;
         }
     }
 }
